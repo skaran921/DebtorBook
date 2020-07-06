1cbfa1b201f065da04dc2bbc630ec34d452a0443
skaran921 <skaran921@gmail.com>
2020-07-06 11:51:58 +0530

    Received Transaction

--- api/Transactions.php
+++ api/Transactions.php
@@ -18,6 +18,33 @@
         $this->conn -> close();
         return $rows;
     }
+
+    // *paymentTransaction
+    public function paymentTransaction($transactionDate,$debtorId,$payAmount,$remark){
+        $userId = $_SESSION["user_auth_id"];
+        $transactionType="P";
+        $sql="INSERT INTO transaction (TRANSACTION_DATE,DEBTOR_ID,USER_ID,PAY_AMOUNT,TRANSACTION_REMARK,TRANSACTION_TYPE)VALUES('$transactionDate','$debtorId','$userId','$payAmount','$remark','$transactionType')";
+        return $this->conn->query($sql);        
+    }
+
+
+    // *receivedTransaction
+    public function receivedTransaction($transactionDate,$debtorId,$receivedAmount,$remark){
+        $userId = $_SESSION["user_auth_id"];
+        $transactionType="R";
+        $sql="INSERT INTO transaction (TRANSACTION_DATE,DEBTOR_ID,USER_ID,RECEIVED_AMOUNT,TRANSACTION_REMARK,TRANSACTION_TYPE)VALUES('$transactionDate','$debtorId','$userId','$receivedAmount','$remark','$transactionType')";
+        return $this->conn->query($sql);        
+    }
+
+    /*
+       SELECT t.TRANSACTION_ID, t.PAY_AMOUNT, t.RECEIVED_AMOUNT,
+    (SELECT SUM(t2.RECEIVED_AMOUNT) - SUM(t2.PAY_AMOUNT) FROM transaction t2 WHERE   t2.USER_ID =1 AND t2.DEBTOR_ID =2 AND  t2.TRANSACTION_ID <= t.TRANSACTION_ID) BALANCE,
+    debtors.DEBTOR_NAME,debtors.DEBTOR_MOBILE,debtors.DEBTOR_EMAIL,debtors.DEBTOR_ADDRESS
+        FROM transaction t
+        LEFT JOIN debtors on t.DEBTOR_ID = debtors.DEBTOR_ID 
+        where t.USER_ID =1 AND t.DEBTOR_ID =2 AND debtors.DEBTOR_STATUS =1
+    */ 
+
  }
 
 ?>
--- user/sideBar.php
+++ user/sideBar.php
@@ -9,6 +9,7 @@
           $dashboardUri ="/DebtorBook/user/"; 
           $debtorUrl = "/DebtorBook/user/debtors/debtors.php";
           $paymentUri = "/DebtorBook/user/transactions/payment.php";
+          $receivedUri = "/DebtorBook/user/transactions/received.php";
           // its give active-link class if current Uri is same the link uri
           function isActiveUri($linkUri,$currentUri){               
                if($linkUri === $currentUri){
@@ -21,7 +22,7 @@
        <a href="<?php echo $host.$dashboardUri;?>" class="<?php isActiveUri($dashboardUri,$currentUri);?>"> <span class="fa fa-dashboard"></span>  Dashboard</a>
        <a href="<?php echo $host.$debtorUrl;?>" class="<?php isActiveUri($debtorUrl,$currentUri);?>"> <span class="fa fa-users"></span> Debtors</a>
        <a href="<?php echo $host.$paymentUri;?>" class="<?php isActiveUri($paymentUri,$currentUri);?>"><span class="fa fa-money"></span> Payment</a>
-       <a href="#home"> <span class="fa fa-rupee-sign"></span> Received</a>
+       <a href="<?php echo $host.$receivedUri;?>" class="<?php isActiveUri($receivedUri,$currentUri);?>"> <span class="fa fa-rupee-sign"></span> Received</a>
        <a href="#home"><span class="fa fa-book"></span> Day Book</a>
        <a href="#home"><span class="fa fa-list-alt"></span> Reports</a>
        <a href="#home"> <span class="fa fa-bell"></span> Reminder</a>
